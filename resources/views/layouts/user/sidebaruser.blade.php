96031fbc72752f2ee79543806792675a1c6fe366
BonifasiusEkky <boniekky.gmail.com>
2024-05-18 23:23:57 +0700

    Sidebar Penduduk

--- resources/views/layouts/user/sidebaruser.blade.php
+++ resources/views/layouts/user/sidebaruser.blade.php
@@ -1,30 +1,29 @@
 <ul class="nav">
     <li class="nav-item">
-      <a class="nav-link" href="{{ url('/penduduk') }}">
-        <i class="mdi mdi-account menu-icon"></i>
-        <span class="menu-title">Home</span>
-      </a>
+        <a class="nav-link" href="{{ url('/penduduk') }}">
+            <i class="mdi mdi-account menu-icon"></i>
+            <span class="menu-title">Home</span>
+        </a>
     </li>
-  
+
     <li class="nav-item">
-      <a class="nav-link" href="{{ url('/penduduk/datakk') }}">
-        <i class="mdi mdi-account-group menu-icon"></i>
-        <span class="menu-title">Lihat Data Keluarga</span>
-      </a>
+        <a class="nav-link" href="{{ url('/penduduk/datakk') }}">
+            <i class="mdi mdi-account-group menu-icon"></i>
+            <span class="menu-title">Lihat Data Keluarga</span>
+        </a>
     </li>
-  
+
     <li class="nav-item">
-      <a class="nav-link" href="{{ url('/user/bansos') }}">
-        <i class="mdi mdi-cash-plus menu-icon"></i>
-        <span class="menu-title">Pengajuan Bansos</span>
-      </a>
+        <a class="nav-link" href="{{ url('/user/bansos') }}">
+            <i class="mdi mdi-cash-plus menu-icon"></i>
+            <span class="menu-title">Pengajuan Bansos</span>
+        </a>
     </li>
-  
+
     <li class="nav-item">
-      <a class="nav-link" href="{{ url('/user/pengaduan') }}">
-        <i class="mdi mdi-message-alert menu-icon"></i>
-        <span class="menu-title">Pengaduan</span>
-      </a>
+        <a class="nav-link" href="{{ url('/penduduk/pengaduan') }}">
+            <i class="mdi mdi-message-alert menu-icon"></i>
+            <span class="menu-title">Pengaduan</span>
+        </a>
     </li>
-  </ul>
-  
+</ul>
